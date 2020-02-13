443a6a91947f504be159bb336916e63532dbd863
itPoet <i@itpoet.cn>
2020-02-13 18:29:13 +0800

    使用application.yml配置项目数据

--- src/main/java/cn/itpoet/springbootluckymoney/HelloController.java
+++ src/main/java/cn/itpoet/springbootluckymoney/HelloController.java
@@ -1,19 +1,36 @@
 package cn.itpoet.springbootluckymoney;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
+// @Controller + @ResponseBody = @RestController
 @RestController
 public class HelloController {
+
+    /* 载入配置文件里面的数据方法1：适用于较少配置项 */
+    /*
+    @Value("${minMoney}")
+    private BigDecimal minMoney;
+
+    // 载入配置文件里面的数据
+    @Value("${description}")
+    private String desc;
+    */
+
+    /* 载入配置文件里面的数据方法2：当需要载入多个配置文件数据时，采用此方法，简便很多 */
+    @Autowired
+    private LimitConfig limitConfig;
 
     /* @GetMapping GET请求 */
     // 旧版写法 @RequestMapping(value = "/hello", method = RequestMethod.GET)
     // 等同于新版写法 @GetMapping("/hello")
-    @GetMapping("/hello")
+    @GetMapping("/say")
     public String say() {
-        return "O(∩_∩)O哈哈~";
+        return "描述：" + limitConfig.getDescription();
     }
-
-
 }
---
+++ src/main/java/cn/itpoet/springbootluckymoney/LimitConfig.java
@@ -0,0 +1,43 @@
+package cn.itpoet.springbootluckymoney;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@ConfigurationProperties(prefix = "limit")
+public class LimitConfig {
+
+    private BigDecimal minMoney;
+
+    private BigDecimal maxMoney;
+
+    private String description;
+
+    public BigDecimal getMinMoney() {
+        return minMoney;
+    }
+
+    /* Mac IDEA 快速生成 get set 方法的快捷键：control + n */
+
+    public void setMinMoney(BigDecimal minMoney) {
+        this.minMoney = minMoney;
+    }
+
+    public BigDecimal getMaxMoney() {
+        return maxMoney;
+    }
+
+    public void setMaxMoney(BigDecimal maxMoney) {
+        this.maxMoney = maxMoney;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
