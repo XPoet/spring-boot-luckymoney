9950281fd401ab1d696cc56e586597479ecd196c
itPoet <i@itpoet.cn>
2020-02-13 09:50:27 +0800

    spring boot project first commit

---
+++ src/main/java/cn/itpoet/springbootluckymoney/SpringBootLuckymoneyApplication.java
@@ -0,0 +1,13 @@
+package cn.itpoet.springbootluckymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLuckymoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLuckymoneyApplication.class, args);
+	}
+
+}
