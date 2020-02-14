bf832a7f41f658b93a1c7c0219d3df5f8feea019
itPoet <i@itpoet.cn>
2020-02-14 13:24:51 +0800

    在LuckymoneyController文件里面编写发红包，领红包，更新红包的接口

---
+++ src/main/java/cn/itpoet/springbootluckymoney/LuckymoneyController.java
@@ -0,0 +1,68 @@
+package cn.itpoet.springbootluckymoney;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class LuckymoneyController {
+
+    @Autowired
+    private LuckymoneyRepository repository;
+
+    /**
+     * 获取红包列表
+     */
+    @GetMapping("/luckymoneys")
+    public List<Luckymoney> list() {
+        return repository.findAll();
+    }
+
+    /**
+     * 创建红包（发红包）
+     */
+    @PostMapping("/create-luckymoney")
+    public Luckymoney create(
+            @RequestParam("producer") String producer,
+            @RequestParam("money") BigDecimal money
+    ) {
+        Luckymoney luckymoney = new Luckymoney();
+        luckymoney.setProducer(producer);
+        luckymoney.setMoney(money);
+        return repository.save(luckymoney);
+    }
+
+
+    /**
+     * 通过id查询红包
+     */
+    @GetMapping("/query-luckymoney/{id}")
+    public Luckymoney findById(
+            @PathVariable("id") Integer id
+    ) {
+        return repository.findById(id).orElse(null);
+    }
+
+    /**
+     * 更新红包（领红包）
+     */
+    @PutMapping("/update-luckymoney/{id}")
+    public Luckymoney update(
+            @PathVariable("id") Integer id,
+            @RequestParam("consumer") String consumer
+    ) {
+
+        Optional<Luckymoney> optional = repository.findById(id);
+        if (optional.isPresent()) {
+            Luckymoney luckymoney = optional.get();
+            luckymoney.setConsumer(consumer);
+            return repository.save(luckymoney);
+        }
+        return null;
+    }
+
+
+}
---
+++ src/main/java/cn/itpoet/springbootluckymoney/LuckymoneyRepository.java
@@ -0,0 +1,7 @@
+package cn.itpoet.springbootluckymoney;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LuckymoneyRepository extends JpaRepository<Luckymoney, Integer> {
+
+}
